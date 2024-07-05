24d6d10526c9e4678ce77ae7d15e0a94dfb58146
Nikhil Kedia <inikhilkedia@gmail.com>
2024-07-05 07:42:30 -0700

    Add custom colors and font family from Figma to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'custom-blue': '#13126C',
+        'custom-dark': '#171731',
+        'custom-gray': '#65657B',
+        'custom-light-gray': '#E7E9EF',
+        'custom-medium-gray': '#6D7088',
+        'custom-error-red': '#C34648',
+        'custom-success-green': '#227C6C',
+      },
+      fontFamily: {
+        georgia: ['Georgia', 'serif'],
+        arial: ['Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [],
